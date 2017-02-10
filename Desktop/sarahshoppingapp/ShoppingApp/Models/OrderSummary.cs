e163e309ff72c93c6d3d795120e95180e88b1a2b
sarahschoonmaker <srschoonmaker@gmail.com>
2017-02-10 14:18:23 -0500

    edited css for main page image

---
+++ Desktop/sarahshoppingapp/ShoppingApp/Models/OrderSummary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingApp.Models
+{
+    public class OrderSummary
+    {
+        public OrderSummary(float v)
+        {
+            SubTotal = v;
+        }
+        public float SubTotal;
+    }
+}
